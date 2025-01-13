35f0a447f8926a091ea7e3fbf01962ff98b5bb34
braziel <braziel@discente.ufg.br>
2025-01-13 12:55:36 -0300

    Post Model, related_name in ForeignKey

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from utils.rands import slugify_new 
 
 class Tag(models.Model):
@@ -64,3 +65,44 @@ class Page(models.Model):
     
     def __str__(self):
         return self.title
+
+
+class Post(models.Model):
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+        
+    title = models.CharField(max_length=65)
+    slug = models.SlugField(
+        unique=True, default=None,
+        null=True, blank=True, max_length=255
+    )
+    excerpt = models.CharField(max_length=150)
+    is_published = models.BooleanField(default=False, help_text='This field must be checked to display this post')
+    content = models.TextField()
+    cover = models.ImageField(upload_to='posts/%Y/%m/', blank=True, default=None)
+    cover_in_post_content = models.BooleanField(default=True, help_text='This field must be checked to display the cover of the post')
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, blank=True, null=True, related_name='post_created_by'
+    )
+    updated_at = models.DateTimeField(auto_now=True)
+    updated_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, blank=True, null=True, related_name='post_updated_by'
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, blank=True, null=True, default=None
+    )
+    tags = models.ManyToManyField(
+        Tag, blank=True, default=""
+    )
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.title)
+            
+        return super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.title
+        
